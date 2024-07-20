d0fc38c64c1db9939649b3d60d3649de39e0ce09
Swetang Pandit <swetangpandit@gmail.com>
2024-07-20 18:40:24 +0530

    Debugging and Custom UI Component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,9 +1,66 @@
-const CourseIdPage = ({params}:{
-    params: { courseId: String }
+import { IconBadge } from "@/components/iconBadge"
+import { db } from "@/lib/db_connect"
+import { auth } from "@clerk/nextjs/server"
+import { LayoutDashboard } from "lucide-react"
+import { redirect } from "next/navigation"
+
+const CourseIdPage = async ({
+  params
+}:{
+    params: { courseId: string }
 }) => {
+
+  const { userId } = auth()
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+  ];
+
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`
+  
+
   return (
-    <div>
-      Course Id: {params.courseId}
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1>
+            Course setup
+          </h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields: {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">
+              Customize your course
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
