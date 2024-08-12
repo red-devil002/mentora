757a098c78ae2dc5cbb0b93986deb999cbf4e964
Swetang Pandit <swetangpandit@gmail.com>
2024-08-12 06:57:23 +0530

    Stripe payment gateway

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,46 @@
+import { db } from "@/lib/db_connect";
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers"
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(
+    req: Request
+) {
+    const body = await req.text()
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!,
+        )
+    } catch (error: any) {
+        return new NextResponse(`WEBHOOK_ERROR: ${error.message}`, {status: 400})
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+    const userId = session?.metadata?.userId;
+    const courseId = session?.metadata?.courseId;
+
+    if (event.type === "checkout.session.completed") {
+        if(!userId || !courseId) {
+            return new NextResponse(`WEBHOOK_ERROR: missing metadata`, {status: 400})
+        }
+
+
+        await db.purchase.create({
+            data:{
+                courseId: courseId,
+                userId: userId,
+            }
+        })
+    } else{
+        return new NextResponse(`WEBHOOK_ERROR: unhandled event type ${event.type}`, {status: 200})
+    }
+
+    return new NextResponse(null, {status: 200})
+}
