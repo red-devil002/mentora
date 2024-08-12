3caddbd0d5cc6f871b4c682521be3133ac038691
Swetang Pandit <swetangpandit@gmail.com>
2024-08-12 05:50:59 +0530

    Course Purchase UI

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,6 +3,10 @@ import { Banner } from "@/components/Banner";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/VideoPlayer";
+import { CourseEnrollButton } from "./_components/CourseEnrollButton";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+import { File } from "lucide-react";
 
 const ChapterIdPage = async ({
     params,
@@ -62,6 +66,50 @@ const ChapterIdPage = async ({
                         isLocked = {isLocked}
                         completeOnEnd  = {completeOnEnd}
                     />
+                </div>
+                <div>
+                    <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+                        <h2 className="text-2xl font-semibold mb-2">
+                            {chapter.title}
+                        </h2>
+                        {purchase ? (
+                            // Add courseProgressButton
+                            <div>
+                                CourseProgressButton
+                            </div>
+                        ) : (
+                            <CourseEnrollButton 
+                                courseId = {params.courseId}
+                                price = {course.price!}
+                            />
+                        )}
+                    </div>
+                    <Separator />
+                    <div>
+                        <Preview 
+                            value={chapter.description}
+                        />
+                    </div>
+                    {!!attachments.length && (
+                        <>
+                            <Separator />
+                            <div className="p-4">
+                                {attachments.map((attachment) => (
+                                    <a 
+                                        href={attachment.url}
+                                        key={attachment.id}
+                                        target="_blank"
+                                        className="flex items-center w-full bg-sky-200 p-3 border text-sky-700 rounded-md hover:underline"
+                                    >
+                                        <File />
+                                        <p className="line-clamp-1 ">
+                                            {attachment.name}
+                                        </p>
+                                    </a>
+                                ))}
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div> 
